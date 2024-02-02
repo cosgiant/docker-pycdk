26569f3f5d1816a20912bdec48a20825fc6b0965
Ahsen Baig <146462+AhsenBaig@users.noreply.github.com>
2024-02-02 09:53:20 -0800

    Add: Dev setup and overwrites.

--- cdk-bash.sh
+++ cdk-bash.sh
@@ -37,6 +37,7 @@ fi
 echo    # Add a newline
 
 export REGISTRY='ghcr.io/cumulus-technology'
+# export REGISTRY='ghcr.io/<repo-fork>'
 
 #Use this env var to control the CDK version. "latest" can be replaced with a version number.
 export PYCDKVER=latest
